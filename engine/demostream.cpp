3116821a408e31119c34cac0c432919f3b3724ce
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-16 17:18:12 +0800

    Delete engine directory

--- engine/demostream.cpp
+++
@@ -1,8 +0,0 @@
-//========= Copyright (c), Valve Corporation, All rights reserved. ============//
-
-#ifndef DEMO_STREAM_HDR
-#define DEMO_STREAM_HDR
-
-
-
-#endif // DEMO_STREAM_HDR
--- engine/host_steaminterface.h
+++
@@ -1,8 +0,0 @@
-//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
-//
-// Purpose:  exposes the steam interface to the engine
-//
-//=============================================================================//
-
-#include "SteamInterface.h"
-extern ISteamInterface *steam;
